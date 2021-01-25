89fda3def4c76d8e05dd4bc4632b1c42c17865b5
Alan Massaro <alan.massar@gmail.com>
2021-01-25 13:32:27 +0100

    My first CLI node app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# CLI-nager
+# Holidates
+
+Display annual holidays of any country.
+
+## Install
+
+`$ npm i @macmowl/holidates`
+
+## Usage
---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const { getCode } = require('country-list')
+const chalk = require('chalk')
+const axios = require('axios').default;
+const figlet = require('figlet');
+const myArgs = process.argv.slice(2);
+
+let year;
+if (myArgs[1]) {
+    year = myArgs[1]
+} else {
+    year = new Date().getFullYear();
+}
+
+axios.get(`https://date.nager.at/api/v2/publicholidays/${year}/${getCode(myArgs[0])}`)
+.then(function (response) {
+    figlet(`${myArgs[0]}'s holidays`, (err, data) => {
+        if (err) {
+            console.log('Oups');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+        console.log(chalk.blue('-----------------------'));
+        response.data.forEach(holiday => {
+            console.log(`${chalk.green.bold(holiday.date)} => ${holiday.name}`);
+        });
+        console.log(chalk.blue('-----------------------'));
+    });
+    
+    
+})
+.catch(function (error) {
+    console.log('An error occured', error.data);
+});
